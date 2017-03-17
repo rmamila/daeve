acb238bd7916f37c5ebc43973c587e99aca57887
cariyaratna <cariyaratna@PEXA-LT-336.local>
2017-03-17 12:42:20 +1100

    added stubs

--- src/main/java/au/pexa/hack/daeve/service/UserActionSuggestionsService.java
+++ src/main/java/au/pexa/hack/daeve/service/UserActionSuggestionsService.java
@@ -2,9 +2,15 @@ package au.pexa.hack.daeve.service;
 
 
 import au.pexa.hack.daeve.model.Suggestion;
+import au.pexa.hack.daeve.model.UserNavigationData;
 import au.pexa.hack.daeve.repositories.UserActionSuggestionsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 
 @Component
 public class UserActionSuggestionsService {
@@ -12,12 +18,38 @@ public class UserActionSuggestionsService {
     @Autowired
     private UserActionSuggestionsRepository userActionSuggestionsRepository;
 
+    @Autowired
+    private UserNavigationService userNavigationService;
+
     public String saveUserSuggestion(final Suggestion suggestion){
         return userActionSuggestionsRepository.save(suggestion).getId();
     }
 
     public Suggestion getSuggestionsFor(final String username) {
+        Collection<UserNavigationData> userNavigationData = userNavigationService.getUserNavigationBy(username);
+        Collections.sort(userNavigationData, (navDataOne, navDataTwo) -> navDataOne.getTimestamp().compareTo(navDataTwo.getTimestamp()));
+
+
         return userActionSuggestionsRepository.findByUsername(username);
     }
+
+    private boolean isNewUser(Collection<UserNavigationData> userNavigationData ){
+        return false;
+    }
+
+    public HashMap<String, Integer> getUserGoal(Collection<UserNavigationData> userNavigationData ){
+        return null;
+
+    }
+
+    private String getLastAccessedPage(){
+        return null;
+    }
+
+    private String getLastAccessedSystem(){
+        return null;
+    }
+
+
 
 }
